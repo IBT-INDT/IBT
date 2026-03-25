f256c53d1f6c96bc71fb848c83be6228999353d5
IBT-INDT <instamillionloto@gmail.com>
2026-03-25 03:01:46 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,28 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('ibt-v1').then((cache) => cache.addAll(['index.html', 'manifest.json'])));
+const CACHE_NAME = 'ibt-mesh-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/admin.html',
+    '/style.css',
+    '/script.js',
+    '/manifest.json',
+    'https://unpkg.com/feather-icons',
+    'https://cdn.jsdelivr.net/npm/gun/gun.js',
+    'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS);
+        })
+    );
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
